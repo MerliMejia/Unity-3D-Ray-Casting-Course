23e15b3c1b6ec7de3cf3ba51c413a860d706613b
Merli Mejia <merlimejia25@gmail.com>
2020-03-24 18:47:49 -0400

    Drawing from the camera to the world

---
+++ Assets/Scripts/DrawFromTheCameraToWorld.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawFromTheCameraToWorld : MonoBehaviour
+{
+    public float distance;
+    void Update()
+    {
+        /*
+         ScreenPointToRay returns a ray going from camera through a screen point.
+         Because we use the mouse position as the screen point the ray will go from
+         the camera to the mouse position.
+         */
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        /*
+         -We draw the ray on the unity editor for debugging.
+         -Every time you multiply a vector by a number, the vector keeps the same direction.
+         So we're basically saying "keep adding to this direction until you reach the target"
+         */
+        Debug.DrawRay(ray.origin, ray.direction * distance, Color.magenta);
+    }
+}
