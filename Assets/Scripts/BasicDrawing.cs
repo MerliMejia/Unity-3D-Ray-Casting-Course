33e0b1815b8799c2269ea58c881651c54b5cc0f7
Merli Mejia <merlimejia25@gmail.com>
2020-03-24 18:02:43 -0400

    basic drawing

---
+++ Assets/Scripts/BasicDrawing.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class BasicDrawing : MonoBehaviour
+{
+    public Transform target;
+    void Update()
+    {
+        /*
+         The direction in between 2 vectors can be calculated by getting the negative value of 
+         the subtraction of the two vectors.
+        */
+        Vector3 direction = -(transform.position - target.position);
+
+        //We create a ray at this object's position and use the direction in between this object and the target.
+        Ray ray = new Ray(transform.position, direction);
+
+        //We draw the ray on the unity editor for debugging.
+        Debug.DrawRay(ray.origin, ray.direction, Color.black);
+        
+    }
+}
