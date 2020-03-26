b865b7cb2b49a93a095f84f0da4c0a68aeb21e1a
Merli Mejia <merlimejia25@gmail.com>
2020-03-26 14:08:21 -0400

    Player movements and aiming

---
+++ Assets/Scripts/Examples/Shooting Platforming/Gun.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Gun : MonoBehaviour
+{
+    public float aimingThickness;
+    Ray aimingRay;
+    Ray cameraToMouseRay;
+
+    Vector3 mousePosition;
+
+    GameObject cannon;
+
+    private void Start()
+    {
+        cannon = transform.GetChild(0).gameObject;
+    }
+
+    private void LateUpdate()
+    {
+        cameraToMouseRaycasting();
+        aimingRaycasting();
+    }
+
+    void aimingRaycasting()
+    {
+        Vector3 origin = cannon.transform.position + new Vector3(cannon.transform.localScale.x / 2, 0, 0);
+        Vector3 mousedirection = -(origin - new Vector3(mousePosition.x, mousePosition.y, 0)).normalized;
+        aimingRay = new Ray(origin, mousedirection);
+        float mouseDistance = Vector3.Distance(origin, mousePosition);
+        Debug.DrawRay(aimingRay.origin, aimingRay.direction * mouseDistance, Color.red);
+    }
+
+    void cameraToMouseRaycasting()
+    {
+        Vector3 playerPosition = transform.parent.parent.position;
+        cameraToMouseRay = Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, playerPosition.z));
+        float distance = Vector3.Distance(Camera.main.transform.position, playerPosition);
+        mousePosition = cameraToMouseRay.GetPoint(distance);
+        Debug.DrawRay(cameraToMouseRay.origin, cameraToMouseRay.direction * distance, Color.blue);
+    }
+}
---
+++ Assets/Scripts/Examples/Shooting Platforming/Player.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour
+{
+
+    public float speed;
+    public float jumpForce;
+    public Gun gun;
+    Rigidbody rg;
+
+    private Ray downRay;
+    private int jumpCounter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rg = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+        downRaycasting();
+    }
+
+    void downRaycasting()
+    {
+        downRay = new Ray(this.transform.position, Vector3.down);
+        RaycastHit hit;
+        GameObject playerBody = transform.GetChild(0).gameObject;
+        float distance = playerBody.transform.localScale.y / 2;
+
+        if(Physics.Raycast(downRay, out hit, distance, 1 << 9))
+        {
+            Debug.DrawRay(downRay.origin, downRay.direction * distance, Color.green);
+            jumpCounter = 0;
+        }
+        else
+        {
+            if(jumpCounter < 1)
+            {
+                Debug.DrawRay(downRay.origin, downRay.direction * distance, Color.green);
+            }
+            else
+            {
+                Debug.DrawRay(downRay.origin, downRay.direction * distance, Color.red);
+            }
+        }
+    }
+
+    void move()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCounter < 1)
+        {
+            rg.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            jumpCounter++;
+        }
+    }
+}
